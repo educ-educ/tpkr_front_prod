99ec62fc577690238247eeddb0f1a5a6fdc67f5b
ledrose <embre@bk.ru>
2023-05-12 19:09:02 +0700

    Added links to navbar

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -3,38 +3,7 @@ import './Footer.css'
 export default function Footer() {
     return (
         <div className='container-fluid bg-green'>
-            {/* <footer className='py-3'>
-                <div className='row justify-content-center'>
-                    <div class="col-6 col-md-2 mb-3">
-                        <h5>Наша команда</h5>
-                        <ul class="nav flex-column">
-                        <li class="nav-item mb-2 footer-text">Егор</li>
-                        <li class="nav-item mb-2 footer-text">Василий</li>
-                        <li class="nav-item mb-2 footer-text">Алина</li>
-                        </ul>
-                    </div>
-                    <div class="col-6 col-md-2 mb-3">
-                        <h5>Телефон</h5>
-                        <ul class="nav flex-column">
-                        <li class="nav-item mb-2 footer-text">88003773754</li>
-                        <li class="nav-item mb-2 footer-text">88003773754</li>
-                        <li class="nav-item mb-2 footer-text">88003773754</li>
-                        </ul>
-                    </div>
-                    <div class="col-6 col-md-2 mb-3">
-                        <h5>Почта</h5>
-                        <ul class="nav flex-column">
-                        <li class="nav-item mb-2 footer-text">abc@tpu.ru</li>
-                        <li class="nav-item mb-2 footer-text">abc@tpu.ru</li>
-                        <li class="nav-item mb-2 footer-text">abc@tpu.ru</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className='row justify-content-start border-secondary border-top'>
-                    <p>© 2022 Climov & co, Inc. All rights reserved.</p>
-                </div>
-            </footer> */}
-            <footer className='nav justify-content-center border-top pb-3 mb-3'>
+            <footer className='nav justify-content-center border-top pb-3'>
                 <div className='footer-info'>
                     <div className='footer-info-title'>Контакты</div>
                     <div className='footer-text'>Почта: tve12@tpu.ru</div>
--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -1,24 +1,42 @@
 import './Navbar.css'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from 'react-redux';
 
 export default function Navbar() {
+    const authUser = useSelector(x => x.auth.user);
+    const userBase = (authUser==null) ? (
+        <>
+            <Link to='/auth' className='nav-item px-2'>
+                <div className="btn btn-green" aria-current="page">Регистрация</div>
+            </Link>
+            <Link to='/auth' className='nav-item px-2'>
+                <div className="btn btn-green" aria-current="page">Войти</div>
+            </Link>
+        </>
+    ) : (
+        <>
+            <Link to='/admin/home' className='nav-item px-2'>
+                <div className="btn btn-green">
+                    <FontAwesomeIcon icon={faUser}/>  
+                </div>
+            </Link>
+        </>
+    )
     return (
             <nav className="navbar navbar-expand navbar-light bg-green">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#0">
+                    <Link to='/' className="navbar-brand" href="#0">
                         Navbar
-                    </a>
+                    </Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div className='d-flex flex-row-reverse gap-2'>
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <div className="navbar-nav d-flex flex-row-reverse">
-                                <div className='nav-item px-2'>
-                                    <a className="btn btn-green" aria-current="page" href="./">Регистрация</a>
-                                </div>
-                                <div className='nav-item px-2'>
-                                    <a className="btn btn-green" aria-current="page" href="#0">Войти</a>
-                                </div>
+                                {userBase}
                                 <div className='nav-item px-2 dropdown'>
                                     <a className="btn btn-green dropdown-toggle" href="#0" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Язык
